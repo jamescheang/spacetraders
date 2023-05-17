3e8f157891516e7b86fdf9280f042488574b69ee
jamescheang <53744921+jamescheang@users.noreply.github.com>
2023-05-17 20:50:15 +0800

    can't get tailwind to work

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './src/App';
+import 'tailwindcss/tailwind.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import './tailwind.css';
 import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="bg-blue-500">
       <h1 className="text-purple-600">Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
