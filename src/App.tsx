0e07d2cdb457f2e72fda7b05b305a72edeb50a2c
jamescheang <53744921+jamescheang@users.noreply.github.com>
2024-01-03 17:56:09 +0800

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import './style.css';
 import './tailwind.css';
 import 'tailwindcss/tailwind.css';
+import Card from './components/Card';
 
 export default function App() {
   return (
     <div className="bg-blue-500">
       <h1 className="text-purple-600">Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Card() {
+  return <h2>This is a card</h2>;
+}
+
+export default Card;
